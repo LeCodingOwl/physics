45408288842ee5aef63b7919ddf916180cfc6f4f
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-05-16 22:34:23 -0700

    added new files

--- game.js
+++ game.js
@@ -2,7 +2,17 @@ var config = {
     type: Phaser.Auto,
     width: 1920,
     height: 1080,
-    scene: []
+    scale: {
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        //debug: true
+        
+      }
+    },
+    scene: [Load, ]
   }
   
   var game = new Phaser.Game(config);
