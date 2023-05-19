f25241a2bd6568a4f54127356c827c18628aced1
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-05-19 14:00:18 -0700

    Bottle flip

--- play.js
+++ play.js
@@ -20,18 +20,34 @@ class Play extends Phaser.Scene {
         ground.scaleY = 1;
 
         //create sprite bottle
-        let bottle = this.matter.add.sprite(400, 100, "bottle", null, {
+        this.bottle = this.matter.add.sprite(400, 100, "bottle", null, {
             isStatic: false, 
             chamfer: 0 , 
             render: { sprite: { xOffset: 0, yOffset: 0.30 } }
         }).setBounce(0.4);
-        bottle.scaleX = 4;
-        bottle.scaleY = 4;
+        this.bottle.scaleX = 4;
+        this.bottle.scaleY = 4;
 
         //bottle.body.setMass = 200;
         
-        Phaser.Physics.Matter.Matter.Body.setCentre(bottle.body, { x: 0, y: 23 }, true);
+        Phaser.Physics.Matter.Matter.Body.setCentre(this.bottle.body, { x: 0, y: 23 }, true);
 
-        this.matter.add.mouseSpring({ length: 1, stiffness: 0.5 });
+        //this.matter.add.mouseSpring({ length: 1, stiffness: 0.5 });
+
+
+        //Throw and flip bottle
+        this.input.on('pointerdown', () => 
+        {
+            this.bottle.setVelocity(0, -15);
+        });
+    }
+
+    update() {
+        console.log(this.bottle.velocity);
+        
+        if (this.bottle.velocity < -15)
+        {
+            Phaser.Physics.Matter.Matter.Body.rotate(this.bottle.body, 0.1);
+        }
     }
 }
